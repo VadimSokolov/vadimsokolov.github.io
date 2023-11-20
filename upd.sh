05dddbcb9430be2e1d03f8e5217384c453d699ec
Vadim Sokolov <vsokolov@gmu.edu>
2023-11-20 13:18:25 -0500

    568 ts notes

--- upd.sh
+++ upd.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 # quarto render index.qmd   
-quarto render cv.qmd
-quarto render index.qmd
-quarto render research.qmd
+# quarto render cv.qmd
+# quarto render index.qmd
+# quarto render research.qmd
+# quarto render course/568.qmd
+
 
 # quarto publish --no-render gh-pages
