6702079693af9090c9504c3da897c6e3aac6d1db
Vadim Sokolov <vsokolov@gmu.edu>
2025-05-19 14:54:04 -0400

    website update

--- upd.sh
+++ upd.sh
@@ -8,8 +8,8 @@
 
 # quarto publish --no-render gh-pages
 
-# quarto render index.qmd
-quarto render courses/664.qmd
+quarto render index.qmd
+# quarto render courses/664.qmd
 git pull
 git add .
 git commit -m "website update"
