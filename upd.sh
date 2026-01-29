abe34ec854d5adb9c3fb5410f8131dd801955102
VadimSokolov <vsokolov@gmu.edu>
2026-01-29 12:07:09 -0500

    website update

--- upd.sh
+++ upd.sh
@@ -7,9 +7,9 @@
 # quarto render courses/600.qmd
 
 # quarto publish --no-render gh-pages
-quarto render courses/caio/cursor-setup-guide.qmd
-quarto render courses/caio/final-project-guide.qmd
-quarto render courses/caio/topic-overview-caio.qmd
+# quarto render courses/caio/cursor-setup-guide.qmd
+# quarto render courses/caio/final-project-guide.qmd
+# quarto render courses/caio/topic-overview-caio.qmd
 
 quarto render index.qmd
 git pull
